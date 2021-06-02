f6cfefd62aad8cd461b8987568ba49f39a6da86d
Tyler Long <tyler4long@gmail.com>
2021-06-02 16:28:10 -0700

    Add credentials sample

--- README.md
+++ README.md
@@ -6,6 +6,9 @@
 composer install -vvv
 ```
 
+Rename `credentials.sample.php` to `credentials.php` and edit it to specify credentials.
+
+
 ## Run
 
 ```
---
+++ credentials.sample.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  'clientId'     => '',
+  'clientSecret' => '',
+  'server'       => 'https://platform.ringcentral.com',
+  'username'     => '',
+  'extension'    =>  '',
+  'password'     => '',
+  'receiver'     => ''
+);
